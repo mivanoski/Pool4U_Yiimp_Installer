2d43f38ff3cc9341904b8b5ef36f2878c6080204
mivanoski <44038646+mivanoski@users.noreply.github.com>
2022-04-20 17:59:21 +0200

    Init

---
+++ bootstrap.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+######################################################################################################
+# Source https://mailinabox.email/ https://github.com/mail-in-a-box/mailinabox                       #
+# Updated by Pool4U for crypto use...                                                                #
+# This script is intended to be run like this:                                                       #
+#                                                                                                    #
+# curl https://raw.githubusercontent.com/mivanoski/Pool4U_Yiimp_Installer/master/bootstrap.sh | bash # 
+#                                                                                                    #
+######################################################################################################
+
+
+# Clone the Yiimp Server repository if it doesn't exist.
+
+echo Installing git . . .
+apt-get -q -q update
+apt-get -q -q install -y git < /dev/null
+echo
+
+echo Downloading Yiimp Server Installer v1.0. . .
+git clone https://github.com/mivanoski/Pool4U_Yiimp_Setup "$HOME"/yiimpserver/install < /dev/null 2> /dev/null
+echo
+
+# Start setup script.
+bash $HOME/yiimpserver/install/start.sh
