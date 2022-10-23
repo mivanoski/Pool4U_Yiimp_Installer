ea36fc57e85a69b11c1bfba4864694092ffeda89
mivanoski <44038646+mivanoski@users.noreply.github.com>
2022-10-23 16:14:10 +0200

    ...

--- bootstrap.sh
+++ bootstrap.sh
@@ -18,10 +18,13 @@
 #echo git installed ...
 sudo apt install git -y
 sudo apt install landscape-common -y
+sudo apt install nano -y
 
 echo Downloading Yiimp Server Installer v1.0. . .
 git clone https://github.com/mivanoski/Pool4U_Yiimp_Setup "$HOME"/yiimpserver/install < /dev/null 2> /dev/null
 echo
+
+nano $HOME/yiimpserver/install/settings.conf
 
 # Start setup script.
 bash $HOME/yiimpserver/install/start.sh
