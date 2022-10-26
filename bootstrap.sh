425f34f0f96cf46a1b65627344a8ca7aaca2264a
mivanoski <44038646+mivanoski@users.noreply.github.com>
2022-10-27 01:22:14 +0200

    ...

--- bootstrap.sh
+++ bootstrap.sh
@@ -9,27 +9,28 @@
 #                                                                                                    #
 ######################################################################################################
 
+ESC_SEQ="\x1b["
+COL_RESET=$ESC_SEQ"39;49;00m"
+RED=$ESC_SEQ"31;01m"
+GREEN=$ESC_SEQ"32;01m"
+YELLOW=$ESC_SEQ"33;01m"
+BLUE=$ESC_SEQ"34;01m"
+MAGENTA=$ESC_SEQ"35;01m"
+CYAN=$ESC_SEQ"36;01m"
 
 # Clone the Yiimp Server repository if it doesn't exist.
 
-#echo Installing git ...
-# apt-get -q -q update
-# sudo apt install git -y
-#echo git installed ...
-sudo apt-get purge apache2 -y 
-sudo apt-get purge apache2-* -y 
-sudo apt-get --purge autoremove -y
-sudo apt install git -y
-sudo apt install landscape-common -y
-sudo apt install nano -y
-sudo apt install dialog -y
-sudo apt update -y
-sudo apt upgrade -y
+sudo apt-get install git -y
 
-
-echo Downloading Yiimp Server Installer v1.0. . .
+echo "$CYAN Downloading Yiimp Server Installer v1.0...$COL_RESET"
 git clone https://github.com/mivanoski/Pool4U_Yiimp_Setup "$HOME"/yiimpserver/install < /dev/null 2> /dev/null
-echo
+echo -e "$GREEN Done...$COL_RESET"
+
+# Start install packages
+
+echo -e "$CYAN Installing needed packages for setup to continue...$COL_RESET"
+bash $HOME/yiimpserver/install/install_packages.sh
+echo -e "$GREEN Done...$COL_RESET"
 
 # Start setup script.
 bash $HOME/yiimpserver/install/start.sh
